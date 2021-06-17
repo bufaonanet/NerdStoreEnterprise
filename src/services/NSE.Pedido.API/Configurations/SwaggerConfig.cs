8d4e5afdf90f4c6ee7a2d8c53e875e158901d947
douglas souto de souza <douglas_bufao@yahoo.com.br>
2021-06-17 16:50:23 -0300

    Setup inicial api de pedido

---
+++ src/services/NSE.Pedido.API/Configurations/SwaggerConfig.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace NSE.Pedido.API.Configurations
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfigurations(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "NerdStore Enterprise Pedidos API",
+                    Description = "Esta API faz a parte do Carrinho de produtos",
+                    Contact = new OpenApiContact { Name = "Douglas Souto", Email = "douglas.bufaonanet@outlook.com" }
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o Token JWT desta maneira: Bearer {token}",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            });
+
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            return app;
+        }
+    }
+}
---
+++ src/services/NSE.Pedido.API/Controllers/VoucherController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using NSE.WebApi.Core.Controllers;
+
+namespace NSE.Pedido.API.Controllers
+{
+    [Authorize]
+    public class VoucherController : MainController
+    {
+    }
+}
